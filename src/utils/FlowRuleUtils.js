52728dc5662ae3da847ef87681173d4b7c418de4
tienanh-clevergroup <tienanh147>
2023-06-05 03:44:15 +0700

    min distance post batch flow

--- src/utils/FlowRuleUtils.js
+++ src/utils/FlowRuleUtils.js
@@ -1,5 +1,4 @@
-﻿
-import {getDevices, getHosts, getLinks} from './NetworkUtils';
+﻿import { getDevices, getHosts, getLinks, postBatchFlows } from "./NetworkUtils";
 /*
     return: [Object] && Object = {
         src: [String],
@@ -13,13 +12,12 @@ import {getDevices, getHosts, getLinks} from './NetworkUtils';
         } 
     }
  */
-export function MinDistanceRule(hostList, deviceList, linkList)
-{
+export function MinDistanceRule(hostList, deviceList, linkList) {
     // create adjacent list
     let adList = {};
     let distances = {};
     let trace = {};
-    
+
     const MAX_DISTANCE = 1000000000;
 
     deviceList.forEach((device) => {
@@ -29,68 +27,57 @@ export function MinDistanceRule(hostList, deviceList, linkList)
     hostList.forEach((host) => {
         adList[host.mac] = [];
         host.locations.forEach((link) => {
-            adList[host.mac].push(
-                {
-                    id: link['elementId'],
-                    port: link.port,
-                }
-            );
-            
-            adList[link['elementId']].push(
-                {
-                    id: host.mac,
-                    port: link.port
-                }
-            )
+            adList[host.mac].push({
+                id: link["elementId"],
+                port: link.port,
+            });
+
+            adList[link["elementId"]].push({
+                id: host.mac,
+                port: link.port,
+            });
         });
     });
 
     linkList.forEach((link) => {
-        if(link.state == 'ACTIVE')
-        {
-            adList[link.src.device].push(
-                {
-                    id: link.dst.device,
-                    port: link.src.port,
-                }
-            );
+        if (link.state == "ACTIVE") {
+            adList[link.src.device].push({
+                id: link.dst.device,
+                port: link.src.port,
+            });
         }
     });
-    
+
     // console.log(adList);
-    
+
     // create a map for distance
-    for(let id in adList)
-    {
+    for (let id in adList) {
         distances[id] = {};
-        trace[id] = {}
-        for(let _id in adList)
-        {
+        trace[id] = {};
+        for (let _id in adList) {
             distances[id][_id] = MAX_DISTANCE;
             trace[id][_id] = {
                 id: id,
-                port: null
+                port: null,
             };
         }
         distances[id][id] = 0;
-        
+
         adList[id].forEach((element) => {
             distances[id][element.id] = 1;
             trace[id][element.id].id = id;
             trace[id][element.id].port = element.port;
-        })
+        });
     }
-    
-    
+
     // calculate distance
-    for(let k in adList)
-    {
-        for(let src in adList)
-        {
-            for(let dst in adList)
-            {
-                if(distances[src][dst] > distances[src][k] + distances[k][dst])
-                {
+    for (let k in adList) {
+        for (let src in adList) {
+            for (let dst in adList) {
+                if (
+                    distances[src][dst] >
+                    distances[src][k] + distances[k][dst]
+                ) {
                     distances[src][dst] = distances[src][k] + distances[k][dst];
                     trace[src][dst] = JSON.parse(JSON.stringify(trace[k][dst]));
                 }
@@ -98,94 +85,112 @@ export function MinDistanceRule(hostList, deviceList, linkList)
         }
     }
 
-    
     let output = [];
-    for(let i = 0 ; i < hostList.length; i++)
-    {
+    for (let i = 0; i < hostList.length; i++) {
         let src = hostList[i].mac;
-        for(let j = 0 ; j < hostList.length; j++)
-        {
+        for (let j = 0; j < hostList.length; j++) {
             let dst = hostList[j].mac;
             let path = [];
-            if( i !== j && distances[src][dst] !== MAX_DISTANCE)
-            {
+            if (i !== j && distances[src][dst] !== MAX_DISTANCE) {
                 let v = dst;
                 let child = v;
-                while(v !== src)
-                {
+                while (v !== src) {
                     let parent = trace[src][v].id;
-                    path.push(
-                        {
-                            id: v,
-                            port_out: trace[v][child].port,
-                            port_in: trace[v][parent].port,
-                        }
-                    );
+                    path.push({
+                        id: v,
+                        port_out: trace[v][child].port,
+                        port_in: trace[v][parent].port,
+                    });
                     child = v;
                     v = parent;
                 }
-                path.push(
-                    {
-                        id: v,
-                        port_out: trace[src][v].port,
-                        port_in: trace[v][src].port,
-                    }
-                );
-                
+                path.push({
+                    id: v,
+                    port_out: trace[src][v].port,
+                    port_in: trace[v][src].port,
+                });
+
                 path = path.reverse();
-                
+
                 let rule = {
                     src: null,
                     src_port_out: null,
                     dst: null,
                     dst_port_in: null,
-                    path: []
+                    path: [],
                 };
-                
-                for(let k = 0 ; k< path.length; k++)
-                {
-                    if(k === 0 || k === path.length - 1)
-                    {
-                        if(k === 0)
-                        {
+
+                for (let k = 0; k < path.length; k++) {
+                    if (k === 0 || k === path.length - 1) {
+                        if (k === 0) {
                             rule.src = path[k].id;
-                            rule.src_port_out = path[k+1].port_in;
-                        }
-                        else
-                        {
+                            rule.src_port_out = path[k + 1].port_in;
+                        } else {
                             rule.dst = path[k].id;
                             rule.dst_port_in = path[k].port_in;
                         }
-                    }
-                    else
-                    {
-                        rule.path.push(
-                            {
-                                id: path[k].id,
-                                port_in: path[k].port_in,
-                                port_out: path[k].port_out,
-                            }
-                        );
+                    } else {
+                        rule.path.push({
+                            id: path[k].id,
+                            port_in: path[k].port_in,
+                            port_out: path[k].port_out,
+                        });
                     }
                 }
-                
+
                 output.push(rule);
             }
         }
     }
-    
+
     // console.log(output);
-    
+
     return output;
 }
 
+const ONOS_APPID = "org.onosproject.proxyarp";
 
-// async function test()
-// {
-//     let hostList = await getHosts();
-//     let deviceList = await getDevices();
-//     let linkList = await getLinks();
-//     MinDistanceRule(hostList.data['hosts'], deviceList.data['devices'], linkList.data['links']);
-// }
-//
+async function minDistanceRoute() {
+    let hostList = await getHosts();
+    let deviceList = await getDevices();
+    let linkList = await getLinks();
+    let result = MinDistanceRule(
+        hostList.data["hosts"],
+        deviceList.data["devices"],
+        linkList.data["links"]
+    );
+
+    let flows = [];
+    console.log(result);
+    result.forEach((pathObject) => {
+        pathObject.path.forEach((path) => {
+            flows.push({
+                appId: ONOS_APPID,
+                priority: 10,
+                isPermanent: false,
+                timeout: 60,
+                deviceId: path.id,
+                treatment: {
+                    instructions: [
+                        {
+                            type: "OUTPUT",
+                            port: path.port_out,
+                        },
+                    ],
+                },
+                selector: {
+                    criteria: [
+                        {
+                            type: "ETH_DST",
+                            mac: pathObject.dst,
+                        },
+                    ],
+                },
+            });
+        });
+    });
+    // console.log(flows)
+    postBatchFlows({ flows: flows });
+}
+
 // test();
--- src/utils/NetworkUtils.js
+++ src/utils/NetworkUtils.js
@@ -1,39 +1,63 @@
-import axios from 'axios';
+import axios from "axios";
 
 const ENDPOINTS = {
-    LINKS: '/onos/v1/links',
-    DEVICES: '/onos/v1/devices',
-    HOSTS: '/onos/v1/hosts',
-}
-const address = "192.168.242.131";
+    LINKS: "/onos/v1/links",
+    DEVICES: "/onos/v1/devices",
+    HOSTS: "/onos/v1/hosts",
+    FLOWS: "/onos/v1/flows",
+};
+const address = "192.168.59.151";
 const port = "8181";
 const url = `http://${address}:${port}`;
 const username = "onos";
 const password = "rocks";
 const baseHeaders = {
-    Authorization: `Basic ${btoa(`${username}:${password}`).toString(`base64`)}`
-}
+    Authorization: `Basic ${btoa(`${username}:${password}`).toString(
+        `base64`
+    )}`,
+};
 
 export function doGet(endpoint, headers, params) {
     let sendHeaders = {};
     Object.assign(sendHeaders, baseHeaders, headers);
     return axios.get(`${url}${endpoint}`, {
         headers: sendHeaders,
-        params: params
+        params: params,
     });
 }
 
-export function getDevices()
-{
+export function getDevices() {
     return doGet(ENDPOINTS.DEVICES, {}, {});
 }
 
-export function getLinks()
-{
+export function getLinks() {
     return doGet(ENDPOINTS.LINKS, {}, {});
 }
 
-export function getHosts()
-{
+export function getHosts() {
     return doGet(ENDPOINTS.HOSTS, {}, {});
+}
+
+export function doPost(endpoint, headers, params, data) {
+    let sendHeaders = {};
+    Object.assign(sendHeaders, baseHeaders, headers);
+
+    var config = {
+        method: "post",
+        url: `${url}${endpoint}`,
+        headers: sendHeaders,
+        data: data,
+    };
+    return axios(config)
+}
+
+export function postBatchFlows(data) {
+    // console.log(JSON.stringify(data).length);
+    console.log(data);
+    return doPost(
+        ENDPOINTS.FLOWS,
+        {'Content-Type': "application/json"},
+        {},
+        data
+    );
 }
