f68585075b6d9c870ee8c27626557d41009019de
Bình Minh Trần <minh.tb02102000@gmail.com>
2023-06-04 21:43:03 +0700

    basic get request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const header = {
+    Authorization: 'Basic b25vczpyb2Nrcw==',
+    mode: 'no-cors',
+  }
+  const config = {
+      headers: header
+  }
+  useEffect(() => {
+    axios.get("http://192.168.58.128:8181/onos/v1/devices", {
+      headers: header
+    })
+      .then((response) => {
+        setData(response.data);
+        console.log(data)
+        setError(null);
+      })
+      .catch(setError);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {JSON.stringify(data)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
