150f65867f6ac93becb1f790ff5a314a7fcea204
Bình Minh Trần <minh.tb02102000@gmail.com>
2023-06-05 11:23:26 +0700

    flow input

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardActions from '@mui/material/CardActions';
 import Grid from "@mui/material/Grid";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import Typography from '@mui/material/Typography';
 import { MinDistanceRoute } from '../utils/FlowRuleUtils'
-import { postBatchFlows } from "../utils/NetworkUtils";
+import { getDevices, getHosts, postBatchFlows } from "../utils/NetworkUtils";
 
 function MainPage() {
   const [inputValue, setInputValue] = useState(60);
@@ -16,6 +18,39 @@ function MainPage() {
   const [portOut, setPortOut] = useState();
   const [destinationMac, setDestinationMac] = useState();
   const [timeout, setTimeout] = useState(60);
+  const [hosts, setHosts] = useState([]);
+  const [devices, setDevices] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectCount, setSelectCount] = useState(0);
+
+  const updateFieldChanged = index => e => {
+    console.log('index: ' + index);
+    console.log('property name: '+ e.target.name);
+    let newArr = [...selectedOptions]; // copying the old datas array
+    // a deep copy is not needed as we are overriding the whole object below, and not setting a property of it. this does not mutate the state.
+    newArr[index] = e.target.value; // replace e.target.value with whatever you want to change it to
+    console.log(newArr)
+    setSelectedOptions(newArr);
+  }
+  
+  useEffect(() => {
+    const retrieve = async () => {
+        const hostsList = await getHosts();
+        const devicesList = await getDevices();
+        setHosts(hostsList.data.hosts);
+        setDevices(devicesList.data.devices);
+    }
+    retrieve()
+  }, []);
+
+  useEffect(() => {
+    console.log('selectedOptions is updated:', selectedOptions);
+  }, [selectedOptions]);
+  
+  const addSelectElement = () => {
+    setSelectCount(selectCount + 1);
+    selectedOptions.push(hosts[0].mac);
+  };
 
   const handleSubmitDefault = () => {
     MinDistanceRoute(parseInt(inputValue))
@@ -27,31 +62,8 @@ function MainPage() {
   };
 
   const handleSubmitFlow = () => {
-    let flows = [];
-    flows.push({
-        // appId: ONOS_APPID,
-        priority: 10,
-        isPermanent: false,
-        timeout: isNaN(timeout) ? 60 : timeout,
-        deviceId: deviceId,
-        treatment: {
-            instructions: [
-                {
-                    type: "OUTPUT",
-                    port: portOut,
-                },
-            ],
-        },
-        selector: {
-            criteria: [
-                {
-                    type: "ETH_DST",
-                    mac: destinationMac,
-                },
-            ],
-        },
-    });
-    postBatchFlows({ flows: flows });
+    const allResults = selectedOptions.flat();
+    console.log(allResults)
   }
 
   return (
@@ -80,29 +92,27 @@ function MainPage() {
         </Typography>
         </CardContent>
         <CardContent>
-            <TextField
-                label="Device Id"
-                value={deviceId}
-                onChange={(e) => setDeviceId(e.target.value)}
-            />
-            <TextField
-                label="Port"
-                value={portOut}
-                onChange={(e) => setPortOut(e.target.value)}
-            />
-            <TextField
-                label="Destination MAC"
-                value={destinationMac}
-                onChange={(e) => setDestinationMac(e.target.value)}
-            />
-            <TextField
-                label="Timeout"
-                value={timeout}
-                onChange={(e) => setTimeout(e.target.value)}
-            />
+        {selectedOptions.map((selectedOption, index) => (
+        <div key={index}>
+          <Select
+            value={selectedOptions[index]}
+            onChange={updateFieldChanged(index)}
+          >
+            {hosts.map(host => (
+                <MenuItem key = {host.mac} value={host.mac}>Host: {host.mac}</MenuItem>
+            ))}
+            {devices.map(device => (
+                <MenuItem key = {device.id} value={device.id}>Device: {device.id}</MenuItem>
+            ))}
+          </Select>
+        </div>
+        ))}
         </CardContent>
         <CardActions>
-            <Button variant="contained" onClick={handleSubmitFlow}>Add</Button>
+            <Button variant="contained" onClick={addSelectElement}>
+                Add to Flow
+            </Button>
+            <Button variant="contained" onClick={handleSubmitFlow}>Submit</Button>
         </CardActions>
     </Card>
     </Grid>
